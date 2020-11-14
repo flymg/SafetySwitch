253d6496149a06ab4d02b8b21e39d60692d2a9aa
flymg <marius.grueter@rapid.io>
2020-11-14 16:44:50 +0100

    refactoring

--- LoadingIndicatorSwitch/Switch/LoadingIndicatorSwitch.swift
+++ LoadingIndicatorSwitch/Switch/LoadingIndicatorSwitch.swift
@@ -6,69 +6,69 @@ import UIKit
 
 @IBDesignable
 class LoadingIndicatorSwitch: UIControl {
-
+    
     // MARK: - Initialization
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         addGestures()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         
         addGestures()
     }
-
+    
     override func prepareForInterfaceBuilder() {
         self.isAnimating = true
         self.layoutIfNeeded()
     }
-
+    
     // MARK: - Layout
     override func layoutSubviews() {
         super.layoutSubviews()
-
+        
         self.outerDonutLayer.layout()
         self.layer.addSublayer(outerDonutLayer)
-
+        
         // Layout Indication Layer, adding will be done by animation
         self.loadingIndicatorShapeLayer.layout()
-
+        
         self.loadingIndicatorDonutLayer.layout()
         self.layer.addSublayer(loadingIndicatorDonutLayer)
-
+        
         self.innerCircleLayer.layout()
         self.layer.addSublayer(innerCircleLayer)
     }
-
+    
     // MARK: - Handle Darkmode
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
-            super.traitCollectionDidChange(previousTraitCollection)
-
-            let outerColorSwitched = self.outerColor
-            self.outerColor = outerColorSwitched
-
-            let innerColorSwitched = self.innerColor
-            self.innerColor = innerColorSwitched
-        }
-
+        super.traitCollectionDidChange(previousTraitCollection)
+        
+        let outerColorSwitched = self.outerColor
+        self.outerColor = outerColorSwitched
+        
+        let innerColorSwitched = self.innerColor
+        self.innerColor = innerColorSwitched
+    }
+    
     // MARK: - Animation
     func animateStroke() {
-
+        
         let loadingAnimation = LoadingAnimation(
             direction: loadingDirection, duration: loadingDuration
         )
-
+        
         let strokeAnimationGroup = CAAnimationGroup()
         strokeAnimationGroup.duration = loadingDuration
         strokeAnimationGroup.repeatDuration = .infinity
         strokeAnimationGroup.animations = [loadingAnimation]
-
+        
         loadingIndicatorShapeLayer.add(strokeAnimationGroup, forKey: nil)
         self.layer.addSublayer(loadingIndicatorShapeLayer)
     }
-
+    
     // MARK: - Gestures / Haptics
     private lazy var gestureRecognizer: UILongPressGestureRecognizer? = nil
     
@@ -79,7 +79,7 @@ class LoadingIndicatorSwitch: UIControl {
         self.gestureRecognizer = longPress
         self.addGestureRecognizer(longPress)
     }
-
+    
     @objc
     func longPress(gesture: UILongPressGestureRecognizer) {
         if gesture.state == UIGestureRecognizer.State.began {
@@ -87,16 +87,16 @@ class LoadingIndicatorSwitch: UIControl {
             hapticFeedback.medium()
         }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if touches.first != nil {
             startTransition()
             hapticFeedback.soft()
         }
     }
-
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-
+        
         guard let touchPosition = touches.first?.location(in: self) else { return }
         
         if !self.bounds.contains(touchPosition) {
@@ -110,25 +110,22 @@ class LoadingIndicatorSwitch: UIControl {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         abortTransition()
     }
-
+    
     // MARK: - Transitions
     enum SwitchState {
         case on
         case off
-        case unknown
     }
-
+    
     private var switchState: SwitchState = .off
     private var loadingDirection: LoadingType = .loading
     private let hapticFeedback: HapticFeedbackGenerator = HapticFeedbackGenerator()
     
     /**
-        Updates the View and animate the transitioning process.
-    */
+     Updates the View and animate the transitioning process.
+     */
     func startTransition() {
         switch switchState {
-        case .unknown:
-            break
         case .on:
             indicatorColor = .systemRed
             stateColor = .clear
@@ -139,36 +136,33 @@ class LoadingIndicatorSwitch: UIControl {
             isAnimating = true
         }
     }
-
+    
     /**
-        Cancels the transitioning animation.
-    */
+     Cancels the transitioning animation.
+     */
     func abortTransition() {
         switch switchState {
-        case .unknown:
-            break
         case .on:
             stateColor = .systemGreen
-            isAnimating = false
         case .off:
             stateColor = .systemRed
-            isAnimating = false
         }
+        isAnimating = false
     }
-
+    
+    /**
+     Completes the transitioning animation.
+     */
     func completeTransition() {
         switch switchState {
-        case .unknown:
-            break
         case .on:
             switchState = .off
-            isAnimating = false
             stateColor = .systemRed
         case .off:
             switchState = .on
-            isAnimating = false
             stateColor = .systemGreen
         }
+        isAnimating = false
     }
     
     // MARK: - Properties
@@ -177,7 +171,7 @@ class LoadingIndicatorSwitch: UIControl {
             loadingIndicatorShapeLayer.lineWidth = indicatorWidth
         }
     }
-
+    
     @IBInspectable var deloadingAnimation: Bool = false {
         didSet {
             if deloadingAnimation {
@@ -187,50 +181,50 @@ class LoadingIndicatorSwitch: UIControl {
             }
         }
     }
-
+    
     @IBInspectable var indicatorColor: UIColor = UIColor.systemGreen {
         didSet {
             loadingIndicatorShapeLayer.strokeColor = indicatorColor.cgColor
         }
     }
-
+    
     @IBInspectable var startAngle: CGFloat = 0 {
         didSet {
             loadingIndicatorShapeLayer.startAngle = startAngle
         }
     }
-
+    
     @IBInspectable var stateColor: UIColor = UIColor.clear {
         didSet {
             loadingIndicatorDonutLayer.fillColor = stateColor.cgColor
         }
     }
-
+    
     @IBInspectable var loadingDuration: Double = 1 {
         didSet {
             let temp = loadingDuration
             loadingDuration = temp
         }
     }
-
+    
     @IBInspectable lazy var outerWidth: CGFloat = 0.025 * self.bounds.width {
         didSet {
             outerDonutLayer.thickness = outerWidth
         }
     }
-
+    
     @IBInspectable var outerColor: UIColor = .label {
         didSet {
             outerDonutLayer.fillColor = outerColor.cgColor
         }
     }
-
+    
     @IBInspectable var innerColor: UIColor = .label {
         didSet {
             innerCircleLayer.fillColor = innerColor.cgColor
         }
     }
-
+    
     // MARK: - Layers
     private lazy var outerDonutLayer: DonutShapeLayer = {
         return DonutShapeLayer(x: 0,
@@ -240,7 +234,7 @@ class LoadingIndicatorSwitch: UIControl {
                                fillColor: outerColor
         )
     }()
-
+    
     private lazy var loadingIndicatorShapeLayer: LoadingIndicatorShapeLayer = {
         return LoadingIndicatorShapeLayer(x: outerWidth + 0.5 * indicatorWidth,
                                           y: outerWidth + 0.5 * indicatorWidth,
@@ -250,7 +244,7 @@ class LoadingIndicatorSwitch: UIControl {
                                           strokeColor: indicatorColor,
                                           startAngle: startAngle)
     }()
-
+    
     private lazy var loadingIndicatorDonutLayer: DonutShapeLayer = {
         return DonutShapeLayer(x: outerWidth,
                                y: outerWidth,
@@ -259,15 +253,15 @@ class LoadingIndicatorSwitch: UIControl {
                                fillColor: stateColor
         )
     }()
-
-        private lazy var innerCircleLayer: CircleShapeLayer = {
-            return CircleShapeLayer(x: outerWidth + indicatorWidth,
-                                    y: outerWidth + indicatorWidth,
-                                    diameter: self.bounds.width - 2 * indicatorWidth - 2 * outerWidth,
-                                    fillColor: innerColor
-            )
-        }()
-
+    
+    private lazy var innerCircleLayer: CircleShapeLayer = {
+        return CircleShapeLayer(x: outerWidth + indicatorWidth,
+                                y: outerWidth + indicatorWidth,
+                                diameter: self.bounds.width - 2 * indicatorWidth - 2 * outerWidth,
+                                fillColor: innerColor
+        )
+    }()
+    
     var isAnimating: Bool = false {
         didSet {
             if isAnimating {
